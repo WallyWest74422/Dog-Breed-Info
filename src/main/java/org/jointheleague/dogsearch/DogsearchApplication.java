9c14601a710bcd9dde1387245d951c39d988c5c3
league <rthdgshgdhs>
2024-06-30 10:13:39 -0700

    initial

---
+++ src/main/java/org/jointheleague/dogsearch/DogsearchApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.dogsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsearchApplication.class, args);
+	}
+
+}
